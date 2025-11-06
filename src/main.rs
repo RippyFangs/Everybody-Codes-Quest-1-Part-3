0888679b72d13af9cbeaaf522a6cc75915463b99
RippyFangs <rippyfangs@gmail.com>
2025-11-06 21:10:14 +0000

    Quest 1 Part 3 Complete!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::fs::read_to_string;
+use core::num;
+use std::{fs::read_to_string, mem::swap};
 
 fn main() {
     //ingest data in a raw form
@@ -15,46 +16,35 @@ fn main() {
     let structured_data: Vec<String> = raw_data.lines().map(String::from).collect();
 
     //gives us two vectors, a list of names and a list of commands
-    let names: Vec<&str> = structured_data[0].split(',').collect();
+    let mut names: Vec<&str> = structured_data[0].split(',').collect();
     let commands: Vec<&str> = structured_data[2].split(',').collect();
 
     //follow rules
-
-    //as per the rules we start our journey at 0, this will keep track of what the
-    let mut counter: u32 = 0;
 
     //0 is our minumum, which we'll embody by working in unsigned integers. We can't go equal to or over the maximum number (as equal to is actuall over by 1 due to starting at 0)
     let num_names: u32 = names.len() as u32;
     println!("There are {} names.", num_names);
     //loop through the commands
     for steps in commands {
-        //extract the number of steps
-        /*         let num_steps: u32 = steps
-        .chars()
-        .nth(1)
-        .expect("Unable to get number of steps")
-        .to_digit(10)
-        .expect("Unable to convert to digit"); */
+        let num_steps: u32 = steps[1..]
+            .parse()
+            .expect("Unable to convert instruction to digit");
 
-        let num_steps: u32 = steps[1..].parse().expect("Unable to convert to digit");
+        let mut swap_target: u32 = num_steps % num_names;
 
-        let reduced_steps: u32 = num_steps % num_names;
-
-        //slight optimisation over last part is that we can now just add all the steps up and then mod number of steps and then we have what we need to do
-        //match left or right commands, ensure we don't go over or under
+        //match left or right commands, ensure we don't go under
         match steps.chars().next().expect("Unable to get instruction") {
-            'R' => counter += reduced_steps,
-            'L' => counter += num_names - reduced_steps,
+            'R' => (),
+            'L' => swap_target = num_names - swap_target,
             _ => println!("Scuffed command - check input"),
         };
-        println!(
-            "Number of steps {}, reduced to result {}, new total {}",
-            num_steps, reduced_steps, counter
-        );
+
+        swap_target += num_names - 1;
+        swap_target %= num_names;
+        println!("The swap target is {},", swap_target);
+        names.swap(0, swap_target as usize);
     }
-    counter += num_names;
-    counter %= num_names;
 
     //print result
-    println!("The name is {}", names[counter as usize]);
+    println!("The name is {}", names[0]);
 }
